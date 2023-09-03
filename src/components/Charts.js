3a8b72e799cca75d1d93ecca9d473e0f97aae062
Avtar Prajapati <prajapatiavtar@gmail.com>
2023-09-03 17:27:03 +0530

    initial setup

---
+++ src/components/Charts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const Charts = () => {
+  return (
+    <View>
+      <Text>Charts</Text>
+    </View>
+  );
+};
+
+export default Charts;
